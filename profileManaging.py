5c2a989f31000a4745918de78796412abb839910
IceTea_1273 <a.makutenaite@gmail.com>
2024-04-12 14:35:23 +0300

    new file:   __pycache__/profileManaging.cpython-310.pyc 	modified:   profileManaging.py 	new file:   startProgram.py 	new file:   userProfiles/USERNAMe/USERNAMe.txt 	modified:   userProfiles/first_user/first_user.txt 	deleted:    userProfiles/first_username/first_username.txt

--- profileManaging.py
+++ profileManaging.py
@@ -9,15 +9,17 @@ class CreateUserProfile:
         self._username = input('Create a username: ')
         self._date_of_birth = input('What is your date of birth?(YYYY MM DD): ')
         
+    def creating_user(self):
         if not os.path.exists(f'X:/Python/kursinis/try/userProfiles/{self._username}'):
             os.makedirs(f'X:/Python/kursinis/try/userProfiles/{self._username}')
-            print(f"Folder {self._username} created successfully.")
+            print(f"Welcome, {self._username}. Account created successfully.")
+            with open(f'X:/Python/kursinis/try/userProfiles/{self._username}/{self._username}.txt', 'w') as f:
+                f.write(f'{self._email}\n{self._password}\n{self._username}\n{self._date_of_birth}\n')
+            return True
         else:
-            print(f"Folder '{self._username} already exists.")
-        
-        with open(f'X:/Python/kursinis/try/userProfiles/{self._username}/{self._username}.txt', 'w') as f:
-            f.write(f'{self._email}\n{self._password}\n{self._username}\n{self._date_of_birth}\n')
-
+            print(f"The username '{self._username}' already exists, try logging in or using a different username.")
+            return False
+            
     def display_profile(self):
         print("User Profile:")
         print("Email:", self._email)
@@ -28,7 +30,3 @@ class CreateUserProfile:
 def delete_profile(username):
     os.remove(f'X:/Python/kursinis/try/userProfiles/{username}.txt')
 
-
-
-first_profile = CreateUserProfile()
-first_profile.display_profile()
---
+++ startProgram.py
@@ -0,0 +1,41 @@
+import profileManaging
+
+
+def sign_up():
+    print('Signing up')
+    create_profile = profileManaging.CreateUserProfile()
+    succes = create_profile.creating_user()
+    return succes
+
+def log_in():
+    print('Logging in')
+    
+
+
+def start():
+    running = True
+    while running:
+        print("\n1. Sign up")
+        print("2. Log in")
+        print("3. Exit")
+        choice = input("Choose an option: ")
+        
+        if choice == "1":
+            if sign_up():
+                break
+            else:
+                continue
+            
+        elif choice == "2":
+            log_in()
+            continue
+        
+        elif choice == "3":
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    start()
+    print('Welcome!')
