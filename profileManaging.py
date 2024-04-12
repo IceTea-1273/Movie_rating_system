bc225807c3cdd992eed57672553c9e73fe997380
IceTea_1273 <a.makutenaite@gmail.com>
2024-04-12 20:58:07 +0300

    modified:   __pycache__/profileManaging.cpython-310.pyc 	modified:   profileManaging.py 	modified:   startProgram.py 	new file:   userProfiles/myname/myname.txt

--- profileManaging.py
+++ profileManaging.py
@@ -20,24 +20,33 @@ class CreateUserProfile:
             print(f"The username '{self._username}' already exists, try logging in or using a different username.")
             return False
 
-    def display_profile(self):
-        print("User Profile:")
-        print("Email:", self._email)
-        print("Username:", self._username)
-        print("Date of Birth:", self._date_of_birth)
 
-def user_login():
+def sign_up():
+    print('Signing up')
+    create_profile = CreateUserProfile()
+    succes = create_profile.creating_user()
+    return succes
+
+
+def log_in():
+    print('Logging in')
     username = input('Input your username: ')
     if not os.path.exists(f'X:/Python/kursinis/try/userProfiles/{username}'):
-        print(f"Username {username} doesn't exist, try signing up")
+        print(f"Username '{username}' doesn't exist, try signing up")
+        return False
     else:
-        password = getpass.getpass('Input your password: ')
-        with open(f'X:/Python/kursinis/try/userProfiles/{username}/{username}.txt', 'r') as f:
-            file = f.readlines()
-            while password != file[1]:
-                print('Incorrect password! Try again')
-                continue
-            print(f'Succsesfully logged in, welcome back, {username}!')
+        while True:
+            password = input('Input your password: ')
+            with open(f'X:/Python/kursinis/try/userProfiles/{username}/{username}.txt', 'r') as f:
+                file = f.readlines()
+                if password == file[1].strip():
+                    print(f'Succsesfully logged in, welcome back, {username}!')
+                    break
+                else:
+                    print('Incorrect password! Try again')
+                    continue
+        return True
+
 
 def delete_profile(username):
     os.remove(f'X:/Python/kursinis/try/userProfiles/{username}.txt')
--- startProgram.py
+++ startProgram.py
@@ -1,17 +1,6 @@
 import profileManaging
 
 
-def sign_up():
-    print('Signing up')
-    create_profile = profileManaging.CreateUserProfile()
-    succes = create_profile.creating_user()
-    return succes
-
-def log_in():
-    print('Logging in')
-    profileManaging.user_login()
-
-
 def start():
     running = True
     while running:
@@ -21,20 +10,24 @@ def start():
         choice = input("Choose an option: ")
         
         if choice == "1":
-            if sign_up():
+            if profileManaging.sign_up():
                 break
             else:
                 continue
             
         elif choice == "2":
-            log_in()
-            break
-        
+            if profileManaging.log_in():
+                break
+            else:
+                continue
+            
+            
         elif choice == "3":
             print("Exiting program.")
             break
         else:
             print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     start()
